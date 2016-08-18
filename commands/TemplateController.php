60e16e2677f1032f83c52276459739e0da842b54
Proph Serg <ProphSerg@gmail.com>
2016-08-18 17:15:09 +0600

    18/08/2016 17:15

--- commands/TemplateController.php
+++ commands/TemplateController.php
@@ -10,7 +10,7 @@ namespace app\commands;
 
 use yii\console\Controller;
 use app\common\Convert;
-use PhpOffice\PhpWord\TemplateProcessor;
+use PhpOffice\PhpWord\IOFactory;
 
 /**
  * Команды работы с шаблонами (Word).
@@ -22,9 +22,25 @@ class TemplateController extends Controller {
 	 * @param string $file Файл шаблона
 	 */
 	public function actionInfo($file) {
-		$tp = new TemplateProcessor($file);
-		echo "Show Variables:\n\t" . implode("\n\t", $tp->getVariables());
-		echo "\n";
+		$tp = IOFactory::load($file, 'MsDoc');
+		#var_dump($tp);
+		echo "Show Sections:\n";
+		$i = 0;
+		foreach ($tp->getSections() as $sN => $s) {
+			echo "Section: " . $i++ . "({$sN})\n";
+			echo "\tSectionId: {$s->getSectionId()}\n";
+			echo "\tDocPart: {$s->getDocPart()}\n";
+			echo "\tDocPartID: {$s->getDocPartId()}\n";
+			echo "\tElementId: {$s->getElementId()}\n";
+			$j = 0;
+			foreach ($s->getElements() as $eN => $e) {
+				echo "\tElement: " . $j++ . "({$eN})\n";
+				echo "\t\tClass: " . get_class($e) . "\n";
+				echo "\t\tText: {$e->getText()}\n";
+			}
+		}
+		#echo "Show Sections:\n\t" . implode("\n\t", $tp->getSections());
+		#echo "\n";
 		return 0;
 	}
 
